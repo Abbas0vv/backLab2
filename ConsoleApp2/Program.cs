44831355048f1a145934140e4fcf1e16e8d33a9f
Abbas0vv <ilhamabbasov46@gmail.com>
2025-03-10 12:34:22 +0400

    tasks completed

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,114 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region task 1
+            //int number = int.Parse(Console.ReadLine());
+            //int faktorial = 1;
+            //int theNumber = number;
+            //while (number > 0)
+            //{
+            //    faktorial *= number;
+            //    number--;
+            //}
+
+            //Console.WriteLine($"{theNumber} ededinin faktroiali {faktorial} edir.");
+            #endregion
+
+            #region task 2
+            //int[] numbers = { 7, 8, 14, 61, 24, 10 };
+            //Console.WriteLine(ikiyeBolunen(numbers) + " " + "ikiye bolunenlerin hasilidir!");
+            //static int ikiyeBolunen(int[] numbers)
+            //{
+            //    int hasil = 1;
+
+            //    for (int i = 0; i < numbers.Length; i++)
+            //    {
+            //        if ((numbers[i] % 2) == 0)
+            //        {
+            //            hasil *= numbers[i];
+            //        }
+            //    }
+
+            //    return hasil;
+            //}
+            #endregion
+
+            #region task 3
+            //int[] numbers = { 7, 8, 14, 61, 24, 10 };
+
+            //int sum = 0;
+
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    if ((numbers[i] % 2) == 0)
+            //    {
+            //        sum += numbers[i];
+            //    }
+            //}
+
+            //Console.WriteLine(sum);
+            #endregion
+
+            #region task 4
+
+            //string[] firstWord = { "car", "table", "computer" };
+            //string[] secondWord = { "table", "mouse", "car" };
+            //sameElements(firstWord, secondWord);
+            //static void sameElements(string[] firstWord, string[] secondWord)
+            //{
+
+            //    for (int i = 0; i < firstWord.Length; i++)
+            //    {
+            //        for (int j = 0; j < secondWord.Length; j++)
+            //        {
+            //            if (firstWord[i] == secondWord[j])
+            //            {
+            //                Console.WriteLine(secondWord[j]);
+            //            }
+            //        }
+            //    }
+            //}
+
+            #endregion
+
+            #region task 5 
+
+            //int[] firstnumbers = { 1, 2, 5 };
+            //int[] secondnumbers = { 2, 10 };
+            //Console.WriteLine(hasilitap(firstnumbers,secondnumbers));
+            //static int hasilitap(int[] numbers1, int[] numbers2)
+            //{
+            //    int hasil = 1;
+
+            //    for (int i = 0; i < numbers1.Length; i++)
+            //    {
+            //        for (int j = 0; j < numbers2.Length; j++)
+            //        {
+            //            int number = numbers1[i] * numbers2[j];
+            //            hasil *= number;
+            //        }
+            //    }
+            //    return hasil;
+            //}
+
+            #endregion
+
+            #region task 6
+
+            //string[] names = { "Anar", "Eli", "Yusif", "Arif" };
+            //char letter = 'A';
+
+            //for (int i = 0; i < names.Length; i++)
+            //{
+            //    if (names[i][0] == letter)
+            //        Console.WriteLine(names[i]);
+            //}
+
+            #endregion
+        }
+    }
+}
